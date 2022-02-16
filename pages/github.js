e41c1128782a0de56c8a0825318d16fbd704e241
Francisco Corvalan <Francisco22corvalan@gmail.com>
2022-02-16 03:42:31 -0300

    Actualizacion

--- pages/github.js
+++ pages/github.js
@@ -1,12 +1,51 @@
 import Layout from '../components/Layout'
 
-const Github = () => (
+const Github = ({ user }) => {
 
-    <Layout>
-        <h1>Github</h1>
-    </Layout>
+    return (
+        <Layout>
+            <div className='row'>
+                <div className='col-md-4 offset-md-4'>
+                    <div className='card card-body text-center'>
+                        <h4 className='m-4'>{user.name}</h4>
+                        <img src={user.avatar_url} alt='user_name'></img>
+                        <br></br>
+                        <p>{user.bio}</p>
+                        <a className='btn btn-info mb-3' href={user.html_url}>Github</a>
+                        <a className='btn btn-info' href={user.blog}>Portfolio</a>
+                        
+                    </div>
+                </div>
+            </div>            
+        </Layout>
 
-)
+    )
+}
+
+
+
+
+
+//await = asincrono async
+export async function getServerSideProps() {
+
+    const res = await fetch('https://api.github.com/users/francorvalant')
+    const data = await res.json();
+
+    console.log(data)
+
+
+
+    return {
+        props: {
+            user: data
+        }
+
+        
+    }
+}
+
+
 
 
 export default Github;
