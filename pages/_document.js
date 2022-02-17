79b8c98c11238c50f649e7dfa140bb7300a7916c
Francisco Corvalan <Francisco22corvalan@gmail.com>
2022-02-17 19:12:17 -0300

    actualizacion

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
                     <title>Simple Portfolio</title>
                     <meta name='description' content='FranCorvalan Portfolio WebSite'></meta>
                     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css'/>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
                 </Head>
                 <body>
                     <Main />
@@ -27,4 +27,3 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
-/* SEO -> meta */
